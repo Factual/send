ae6338fccbefa8c22c57459a70c5a56a456ba290
sudovinh <vinh@factual.com>
2020-02-10 13:38:54 -0800

    fix geodb dependency issue

--- server/amplitude.js
+++ server/amplitude.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const geoip = require('fxa-geodb')();
+//const geoip = require('fxa-geodb')();
 const fetch = require('node-fetch');
 const config = require('./config');
 const pkg = require('../package.json');
